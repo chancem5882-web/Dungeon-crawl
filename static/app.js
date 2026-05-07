94765bc60837d9ce295268eee91efaccefa9fa3b
chancem5882-web <chancem5882@gmail.com>
2026-05-07 15:13:24 -0500

    Update app.js

--- static/app.js
+++ static/app.js
@@ -2,11 +2,11 @@ const cid = window.location.pathname.split("/").pop();
 
 function collectData(){
 
-    let data = {};
+    let data = {
+        effects:[]
+    };
 
-    document.querySelectorAll(
-        "input, textarea"
-    ).forEach(el=>{
+    document.querySelectorAll("input, textarea").forEach(el=>{
 
         if(el.name){
             data[el.name] = el.value;
@@ -14,29 +14,44 @@ function collectData(){
 
     });
 
+    document.querySelectorAll(".effect").forEach(e=>{
+
+        data.effects.push({
+
+            name:e.querySelector(".ename").value,
+
+            value:parseInt(
+                e.querySelector(".eval").value
+            ) || 0,
+
+            duration:parseInt(
+                e.querySelector(".edur").value
+            ) || 0,
+
+            type:e.querySelector(".etype").value,
+
+            is_buff:e.querySelector(".ebuff").checked ? 1 : 0
+        });
+
+    });
+
     return data;
 }
 
-async function autosave(){
+async function updateCharacter(){
 
-    await fetch(`/autosave/${cid}`,{
+    await fetch(`/update/${cid}`,{
+
         method:"POST",
+
         headers:{
             "Content-Type":"application/json"
         },
+
         body:JSON.stringify(
             collectData()
         )
     });
 
+    location.reload();
 }
-
-document.addEventListener("input",()=>{
-
-    clearTimeout(window.saveTimer);
-
-    window.saveTimer = setTimeout(()=>{
-        autosave();
-    },400);
-
-});
