4c067faea640534892fc750884694ffcc8ec7f74
chancem5882-web <chancem5882@gmail.com>
2026-05-07 14:25:02 -0500

    Update autosave.js

--- static/autosave.js
+++ static/autosave.js
@@ -1,31 +1,39 @@
-let timer;
+const cid=window.location.pathname.split("/").pop();
 
 function collect(){
-return {
-level:document.querySelector("[name=level]").value,
-hp:document.querySelector("[name=hp]").value,
-max_hp:document.querySelector("[name=max_hp]").value,
-gold:document.querySelector("[name=gold]").value,
-views:document.querySelector("[name=views]").value,
-followers:document.querySelector("[name=followers]").value,
-favorites:document.querySelector("[name=favorites]").value,
-equipment:document.querySelector("[name=equipment]").value,
-spells:document.querySelector("[name=spells]").value,
-inventory:document.querySelector("[name=inventory]").value
-};
+
+let data={effects:[]};
+
+document.querySelectorAll("input,textarea").forEach(el=>{
+if(el.name)data[el.name]=el.value;
+});
+
+document.querySelectorAll(".effect").forEach(e=>{
+
+data.effects.push({
+name:e.querySelector(".ename").value,
+value:parseInt(e.querySelector(".eval").value)||0,
+duration:parseInt(e.querySelector(".edur").value)||0,
+type:e.querySelector(".etype").value,
+is_buff:e.querySelector(".ebuff").checked?1:0
+});
+
+});
+
+return data;
 }
 
-function autosave(cid){
-clearTimeout(timer);
-timer=setTimeout(()=>{
+function autosave(){
+
 fetch(`/autosave/${cid}`,{
 method:"POST",
 headers:{"Content-Type":"application/json"},
 body:JSON.stringify(collect())
 });
-},500);
+
 }
 
-document.querySelectorAll("input,textarea").forEach(el=>{
-el.addEventListener("input",()=>autosave(window.CID));
+document.addEventListener("input",()=>{
+clearTimeout(window.saveTimer);
+window.saveTimer=setTimeout(autosave,400);
 });
